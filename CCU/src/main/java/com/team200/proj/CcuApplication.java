6dcd72cd4bae2186140cc1f8806cd2dd8b50d5d6
김승욱 <63062516+raccoon115@users.noreply.github.com>
2022-08-31 17:55:35 +0900

    KSU-0831-초기 세팅파일 Marge

---
+++ CCU/src/main/java/com/team200/proj/CcuApplication.java
@@ -0,0 +1,13 @@
+package com.team200.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcuApplication.class, args);
+	}
+
+}
