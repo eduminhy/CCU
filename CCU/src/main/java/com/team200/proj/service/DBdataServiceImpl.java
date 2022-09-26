90f7376dc36f1b32bb2649e4227ca2cd05cc7412
김승욱 <63062516+raccoon115@users.noreply.github.com>
2022-09-26 11:50:19 +0900

    KSU-0926-DB데이터 수정

--- CCU/src/main/java/com/team200/proj/service/DBdataServiceImpl.java
+++ CCU/src/main/java/com/team200/proj/service/DBdataServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.team200.proj.dao.DBdataDAO;
 import com.team200.proj.vo.placeVO;
+import com.team200.proj.vo.showVO;
 
 @Service
 public class DBdataServiceImpl implements DBdataService {
@@ -16,5 +17,11 @@ public class DBdataServiceImpl implements DBdataService {
 		// TODO Auto-generated method stub
 //		System.out.println(1234);
 		dao.inputPlaceData(pVO);
+	}
+
+	@Override
+	public void inputShowData(showVO sVO) {
+		// TODO Auto-generated method stub
+		dao.inputShowData(sVO);
 	}
 }
